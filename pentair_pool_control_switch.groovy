d5e9800d083059287f249a82a94913b1998142bf
Brad Sileo <brad@sileo.name>
2019-09-18 16:29:13 -0400

    Added final child devices

---
+++ pentair_pool_control_switch.groovy
@@ -0,0 +1,63 @@
+/**
+ *  Copyright 2019 Brad Sileo
+ *
+ *
+ *  Intellibrite Color Mode Device
+ *
+ *  Author: Brad Sileo
+ *
+ *  Date: 2018-10-21
+ */
+metadata {
+	definition (name: "Pentair Pool Control Switch", namespace: "bsileo", author: "Brad Sileo") {
+		capability "Switch"
+        command "onConfirmed"
+        command "offConfirmed"
+        attribute "friendlyName", "string"
+        attribute "circuitFunction", "string"
+	}
+}
+
+def installed() {	
+}
+
+def parse(String description) {
+	try {
+         def pair = description.split(":")
+         createEvent(name: pair[0].trim(), value: pair[1].trim())
+     }
+     catch (java.lang.ArrayIndexOutOfBoundsException e) {
+           log.debug "Error! " + e   
+    }
+	
+}
+
+def onConfirmed() {
+    //log.debug("CONF ${device} turned on")
+	sendEvent(name: "switch", value: "on", displayed:true)    
+}
+
+def offConfirmed() {
+	//log.debug("CONF ${device} turned off")
+	sendEvent(name: "switch", value: "off", displayed:true)  
+}
+
+def on() {
+	parent.childOn(device.deviceNetworkId)
+    sendEvent(name: "switch", value: "turningOn", displayed:false,isStateChange:false)    
+}
+
+def off() {
+	parent.childOff(device.deviceNetworkId)
+    sendEvent(name: "switch", value: "turningOff", displayed:false,isStateChange:false)
+}
+
+def setFriendlyName(name) {
+   //log.debug("Set FName to ${name}")
+   sendEvent(name: "friendlyName", value: name, displayed:false)
+}
+
+def setCircuitFunction(name) {
+   //log.debug("Set CircuitFunction to ${name}")
+   sendEvent(name: "circuitFunction", value: name, displayed:false)
+}
---
+++ pentair_spa_pump_control.groovy
@@ -0,0 +1,60 @@
+/**
+ *  Copyright 2019 Brad Sileo
+ *
+ *
+ *  Author: Brad Sileo
+ *
+ */
+metadata {
+	definition (name: "Pentair Spa Pump Control", namespace: "bsileo", author: "Brad Sileo") {
+	capability "Switch"
+        command "onConfirmed"
+        command "offConfirmed"
+        attribute "friendlyName", "string"
+        attribute "circuitFunction", "string"
+	}
+}
+
+def installed() {	
+}
+
+def parse(String description) {
+	try {
+         def pair = description.split(":")
+         createEvent(name: pair[0].trim(), value: pair[1].trim())
+     }
+     catch (java.lang.ArrayIndexOutOfBoundsException e) {
+           log.debug "Error! " + e   
+    }
+	
+}
+
+def onConfirmed() {
+    //log.debug("CONF ${device} turned on")
+	sendEvent(name: "switch", value: "on", displayed:true)    
+}
+
+def offConfirmed() {
+	//log.debug("CONF ${device} turned off")
+	sendEvent(name: "switch", value: "off", displayed:true)  
+}
+
+def on() {
+	parent.spaPumpOn()
+    sendEvent(name: "switch", value: "turningOn", displayed:false,isStateChange:false)    
+}
+
+def off() {
+	parent.spaPumpOff()
+    sendEvent(name: "switch", value: "turningOff", displayed:false,isStateChange:false)
+}
+
+def setFriendlyName(name) {
+   //log.debug("Set FName to ${name}")
+   sendEvent(name: "friendlyName", value: name, displayed:false)
+}
+
+def setCircuitFunction(name) {
+   //log.debug("Set CircuitFunction to ${name}")
+   sendEvent(name: "circuitFunction", value: name, displayed:false)
+}
