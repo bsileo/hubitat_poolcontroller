dd7f6e98fb6fbd45d3edd73d7da27a9dc1d9792f
Brad Sileo <brad@sileo.name>
2022-06-07 11:32:31 -0400

    Fix bug that was hiding setting heater mode

--- devicetypes/bsileo/pool-controller-body.src/pool-controller-body.groovy
+++ devicetypes/bsileo/pool-controller-body.src/pool-controller-body.groovy
@@ -6,7 +6,7 @@
  *  Author: Brad Sileo
  *
  *
- *  version: 1.5
+ *  version: 1.6
  */
 
 metadata {
@@ -30,26 +30,21 @@ metadata {
        attribute "waterTemp", "Number"
        attribute "setPoint", "Number"
 
-       if (isHE) {
-           command "setHeaterMode", [[name:"Heater mode*",
-                                      "type":"ENUM",
-                                      "description":"Heater mode to set (from the supported heater modes list)",
-                                      "constraints":["Off", "Heater", "Solar Pref", "Solar Only"]]]
+       command "setHeaterMode", [[name:"Heater mode*",
+                                  "type":"ENUM",
+                                  "description":"Heater mode to set (from the supported heater modes list)",
+                                  "constraints":["Off", "Heater", "Solar Pref", "Solar Only"]]]
 
-           command "setHeaterSetpoint", [[name:"Heater Setpoint*",
-                                      "type":"ENUM",
-                                      "description":"Set the heater set point",
-                                      "constraints":[50,51,52,53,54,55,56,57,58,59,
-                                                     60,61,62,63,64,65,66,67,68,69,
-                                                     70,71,72,73,74,75,76,77,78,79,
-                                                     80,81,82,82,84,85,86,87,88,89,
-                                                     90,91,92,93,94,95,96,97,98,99,
-                                                     100,101,102,103,104]
-                                     ]]
-       } else {
-           // ST version of commands goes here
-
-       }
+       command "setHeaterSetpoint", [[name:"Heater Setpoint*",
+                                  "type":"ENUM",
+                                  "description":"Set the heater set point",
+                                  "constraints":[50,51,52,53,54,55,56,57,58,59,
+                                                 60,61,62,63,64,65,66,67,68,69,
+                                                 70,71,72,73,74,75,76,77,78,79,
+                                                 80,81,82,82,84,85,86,87,88,89,
+                                                 90,91,92,93,94,95,96,97,98,99,
+                                                 100,101,102,103,104]
+                                 ]]
     }
 
 	preferences {         
